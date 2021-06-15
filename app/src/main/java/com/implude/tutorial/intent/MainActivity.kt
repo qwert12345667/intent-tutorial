36a9f4bf3b3fc02119d51c7500625fe856db37eb
qwert12345667 <seonghun20502@gmail.com>
2021-06-16 07:29:42 +0900

    git commit-am "feat: open another activity on button click"

--- app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
+++ app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.implude.tutorial.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_open)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, AnotherActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
